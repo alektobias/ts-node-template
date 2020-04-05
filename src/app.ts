98abe0e962a062659558f70f9a99ab17c25cdbc4
Alek Tobias <alektobias@gmail.com>
2020-04-05 20:41:53 -0300

    feat: exception handler added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import 'dotenv/config';
-import path from 'path';
-import fs from 'fs';
-import express from 'express';
+
+import express, { Response, Request } from 'express';
 import routes from './routes';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
-
 class App {
 	public server: express.Application;
 
@@ -14,12 +12,13 @@ class App {
 		this.server = express();
 		this.middlewares();
 		this.routes();
+		this.exceptionHandler();
 	}
 
 	private middlewares(): void {
 		this.server.use(express.json());
 		this.server.use(helmet());
-		this.server.use(cors());
+		this.server.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
 		this.server.use(
 			morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common')
 		);
@@ -27,6 +26,14 @@ class App {
 
 	private routes(): void {
 		this.server.use(routes);
+	}
+
+	private exceptionHandler(): void {
+		this.server.use(
+			(req: Request, res: Response): Response => {
+				return res.status(500).json({ error: 'Internal server error' });
+			}
+		);
 	}
 }
 
